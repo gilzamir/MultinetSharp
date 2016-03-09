70de327c8337417d4930266df2bd4df553b33f94
Gilzamir Gomes <gilzamir@gmail.com>
2016-03-09 00:29:57 -0300

    BugFix.

--- Multinet/CrossoverImpl.cs
+++ Multinet/CrossoverImpl.cs
@@ -7,8 +7,15 @@ namespace Multinet.Genetic
 {
     public sealed class CrossoverImpl
     {
+        public static int seed = -1;
+        public static int last_seed = -1;
         public static Genome CrossoverMethod(Genome gen1, Genome gen2)
         {
+            if (seed < 0)
+            {
+                seed = unchecked(DateTime.Now.Ticks.GetHashCode());
+            }
+            last_seed = seed;
             Random rnd = new Random();
             Genome child = new Genome();
 
@@ -27,26 +34,29 @@ namespace Multinet.Genetic
                 ICollection<uint> genesKeys = chr1.GetKeys();
                 
                 int m = System.Math.Min(chr1.Size, chr2.Size);
+                int m2 = chr2.Size;
 
                 int    point = (int)rnd.Next(m);
                 
                // Console.WriteLine("POINT: {0}", point);
                 Chromossome nchr = new Chromossome();
 
+                
                 for (int i = 0; i < point; i++)
                 {
                     BitArray copy = BitArrayUtils.DeepClone(chr1.GetGene((uint)i));
                     nchr.AddGene((uint)i, copy);
                 }
 
-                for (int i = point; i < chr2.Size; i++)
+                for (int j = point; j < m2; j++)
                 {
-                    BitArray copy = BitArrayUtils.DeepClone(chr2.GetGene((uint)i));
-                    nchr.AddGene((uint)i, copy);
+                    BitArray copy = BitArrayUtils.DeepClone(chr2.GetGene((uint)j));
+                    nchr.AddGene((uint)j, copy);
                 }
 
                 child.AddChromossome(id, nchr);
             }
+            seed = -1;
             return child;
         }
     }
